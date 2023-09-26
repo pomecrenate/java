bf558d3500cca474ff862a640dcad7bde6d6e894
pomecrenate <kk37325940@gmail.com>
2023-09-26 22:47:08 +0900

    함수형 프로그래밍 공부

---
+++ udemy-java/src/com/udemy/java/functionalprogramming/FPNumberRunner.java
@@ -0,0 +1,34 @@
+package com.udemy.java.functionalprogramming;
+
+import java.util.List;
+
+public class FPNumberRunner {
+
+  public static void main(String[] args) {
+    List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
+
+    numbers.stream()
+    .forEach(element -> System.out.println(element));
+
+    //    normalSum(numbers);
+
+    int sum = fpSum(numbers);
+
+    System.out.println(sum);
+  }
+
+  private static int fpSum(List<Integer> numbers) {
+    return numbers.stream()
+        .filter(number -> number % 2 == 1)
+        .reduce(0, (number1, number2) -> number1 + number2);
+  }
+
+  private static int normalSum(List<Integer> numbers) {
+    int sum = 0;
+    for (int number : numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+
+}
